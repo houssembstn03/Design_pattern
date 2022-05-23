e228b4a925cedac1f5f7c9eebb27bcaf983832eb
houssembstn03 <houssem.bensalha1987@hotmail.com>
2022-05-23 13:14:28 +0100

    Add project files.

---
+++ Design_pattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_pattern
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //test singleton 
+            Singleton s = Singleton.getInstance();
+            s.getName("houssem");
+            s.print();
+
+            Singleton s2 = Singleton.getInstance();
+            s2.print();
+
+            Console.ReadKey(true);  
+        }
+    }
+}
---
+++ Design_pattern/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_pattern
+{
+    // singleton design pattern eager loading
+     sealed class Singleton
+     {
+        private static int counter;
+        private static Singleton instance = new Singleton();
+        public string _name;
+        private Singleton()
+        {
+            counter += 1;
+            Console.WriteLine("instatnce num " + counter);
+
+        }
+
+        public static Singleton getInstance()
+        {
+            
+            return instance;
+
+        }
+
+        public void getName(string name)
+        {
+            _name = name;
+            
+        }
+
+
+        public void print()
+        {
+            if (_name == null)
+            {
+                Console.WriteLine("not found");
+            }
+            else
+            {
+                Console.WriteLine(_name);
+            }
+        }
+
+        
+     }
+}
